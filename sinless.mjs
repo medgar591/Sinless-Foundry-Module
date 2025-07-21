13f9c57783bee4d19817ac0d2f46e5201653966f
Matt Edgar <matthewaedgar@gmail.com>
2025-07-20 19:55:47 -0500

    Expanded initialization and support for handlebars

--- sinless.mjs
+++ sinless.mjs
@@ -13,15 +13,13 @@ import { SINLESS } from "./modules/config.mjs";
 /* -------------------------------------------- */
 /*  Init Hook                                   */
 /* -------------------------------------------- */
+Hooks.once('init', async () => {
 
-Hooks.once('init', function() {
-	// Adding utility classes to global context
-	game.sinless = {
-		SinlessActor,
-	};
+	console.log("SINLESS | Initializing Sinless Core System");
 
-	// Add custom constants for configuration
-	CONFIG.SINLESS = SINLESS
+	// Set up global Config object
+	CONFIG.SINLESS = SINLESS;
+	CONFIG.INIT = true;
 
 	// Assign document classes
 	CONFIG.Actor.documentClass = SinlessActor
@@ -36,4 +34,56 @@ Hooks.once('init', function() {
 		makeDefault: true,
 		label: 'SINLESS.SheetLabels.Actor',
 	});
+
+	// Set up handlebars sheets
+	preloadHandlebarsTemplates();
+	registerHandlebarsHelpers();
 });
+
+/* -------------------------------------------- */
+/*  Ready Hook                                  */
+/* -------------------------------------------- */
+Hooks.once('ready', async () => {
+
+	// Completed initialization, release lock
+	CONFIG.INIT = false;
+
+	// Only do more for GMs
+	if(!game.user.isGM) return;
+});
+
+/* -------------------------------------------- */
+/*  Private Functions                           */
+/* -------------------------------------------- */
+function preloadHandlebarsTemplates() {
+	
+	const templatePaths = [
+		// "systems/sinless/templates/partials/template.hbs"
+	];
+
+	return loadTemplates(tempplatePaths);
+}
+
+function registerHandlebarsHelpers() {
+	Handlebars.registerHelper("equals", function(v1, v2) {return (v1 === v2)});
+	Handlebars.registerHelper("contains", function(element, search) {return (element.includes(search))});
+	Handlebars.registerHelper("concat", function(s1, s2, s3 = "") {return (s1 + s2 + s3)});
+	Handlebars.registerHelper("isGreater", function(p1, p2) {return (p1 > p2)});
+	Handlebars.registerHelper("isGreaterOrEqual", function(p1, p2) {return (p1 >= p2)});
+	Handlebars.registerHelper("ifOr", function(conditinal1, conditional2) {return (conditinal1 || conditional2)});
+	Handlebars.registerHelper("logConsole", function(value) { console.log(value) });
+	Handlebars.registerHelper("toBoolean", function(string) {return (string === "true")});
+	Handlebars.registerHelper("for", function(from, to, incr, content) {
+		let result = "";
+		for (let i = from; i < to; i += incr)
+			result += content.fn(i);
+		return result;	
+	});
+	Handlebars.registerHelper("notEmpty", function(value) {
+		// done to sometimes catch interpreting 0 as null
+		if (value == 0 || value == "0") return true;
+		if (value == null || value == "") return false;
+		return true;
+	});
+	//Handlebars.registerHelper("", function(,) {return ()});
+}
