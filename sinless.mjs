aedba2f6fff7503fc7594cae48e9863d5afebfb1
Matt Edgar <matthewaedgar@gmail.com>
2025-07-26 20:58:10 -0500

    Corrected issues from breaking out new skills page

--- sinless.mjs
+++ sinless.mjs
@@ -63,6 +63,7 @@ function preloadHandlebarsTemplates() {
 	const templatePaths = [
 		"systems/sinless/templates/partials/character-sheet-attributes.hbs",
 		"systems/sinless/templates/partials/character-sheet-skills.hbs",
+		"systems/sinless/templates/partials/character-sheet-skills2.hbs",
 		// "systems/sinless/templates/partials/template.hbs",
 	];
 
